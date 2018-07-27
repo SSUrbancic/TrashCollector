a89c81f7631ed946f47d7f7eb8d01b638edd31b9
Sam <SSUrbancic@outlook.com>
2018-07-26 21:36:20 -0500

    Created a trashpickup model with id, foreign key customer id, date, day of the week, puck up completed bool, and price of the pick up.

---
+++ TrashCollector/Models/TrashPickUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class TrashPickUp
+    {
+        public int pickUpID { get; set; }
+        public int customerID { get; set; }
+        public DateTime date { get; set; }
+        public DayOfWeek dayOfWeek { get; set; }
+        public bool pickUpCompleted { get; set; }
+        public double price { get; set; }
+        
+    }
+}
