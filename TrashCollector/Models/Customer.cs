9dba88025bc898ae4099f73580e954d36b448e65
Sam <SSUrbancic@outlook.com>
2018-07-26 21:24:35 -0500

    Created a customer model with ID, name, address, and payment balance properties.

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string addressLine1 { get; set; }
+        public string addessLine2 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zipCode { get; set; }
+        public double paymentBalance { get; set; }
+    }
+}
